25ced92c9c6edd51b4f4a6d7d07e34e6d91bcfed
Favre Estrada <favreestrada@gmail.com>
2018-09-30 13:53:36 -0600

    Fixing typescript bug

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import Main from './containers/main/main';
 
 const ele = document.getElementById("root");
 ReactDOM.render(
-	<div></div>, ele
+	<Main />, ele
 );
